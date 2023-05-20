a8ad6af0e6ec8e917c607e9954855576fc8a248a
RafayMemon01 <rafayrashid200611@gmail.com>
2023-05-20 16:00:40 +0500

    UI DONE

---
+++ node.js
@@ -0,0 +1,7 @@
+let mainHeading = document.querySelector(".mainH1").style.color="red",justifyContent="flex";
+window.addEventListener('load', function() {
+    let content = document.querySelector('.content');
+    content.style.display = 'block';
+  });
+    
+
